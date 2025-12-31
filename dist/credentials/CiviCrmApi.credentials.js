9098bfc22e85e490690c0d4eaa001256b30d542f
jreartes-ixiam <j.reartes@ixiam.com>
2025-12-31 13:51:16 +0100

    version 1.1.30

--- dist/credentials/CiviCrmApi.credentials.js
+++ dist/credentials/CiviCrmApi.credentials.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CiviCrmApi = void 0;
+exports.civiCrmApi = exports.CiviCrmApi = void 0;
 class CiviCrmApi {
     constructor() {
         this.name = 'civiCrmApi';
@@ -8,16 +8,16 @@ class CiviCrmApi {
         this.documentationUrl = 'https://docs.civicrm.org/dev/en/latest/api/v4/usage/#auth';
         this.properties = [
             {
-                displayName: 'URL',
-                name: 'url',
+                displayName: 'Base URL',
+                name: 'baseUrl',
                 type: 'string',
                 default: '',
                 placeholder: 'https://example.org/civicrm',
                 required: true,
             },
             {
-                displayName: 'API Key',
-                name: 'apiKey',
+                displayName: 'API Token',
+                name: 'apiToken',
                 type: 'string',
                 typeOptions: {
                     password: true,
@@ -30,14 +30,13 @@ class CiviCrmApi {
             type: 'generic',
             properties: {
                 headers: {
-                    Authorization: 'Bearer ={{$credentials.apiKey}}',
+                    'X-Civi-Auth': '={{ "Bearer " + $credentials.apiToken }}',
                 },
             },
         };
         this.test = {
             request: {
-                baseURL: '={{$credentials.url}}',
-                url: '/civicrm/ajax/api4/Contact/get',
+                url: '={{ $credentials.baseUrl }}/civicrm/ajax/api4/Contact/get',
                 method: 'POST',
                 body: {
                     version: 4,
@@ -50,3 +49,4 @@ class CiviCrmApi {
     }
 }
 exports.CiviCrmApi = CiviCrmApi;
+exports.civiCrmApi = CiviCrmApi;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import type { INodeType, ICredentialType } from 'n8n-workflow';
 
 import CiviCrmNode from './nodes/CiviCrm/CiviCrm.node';
-import { CiviCrmApi } from './credentials/CiviCrmApi.credentials';
+import { CiviCrmApi } from '../credentials/CiviCrmApi.credentials';
 
 export const nodes: INodeType[] = [
 	new CiviCrmNode(),
